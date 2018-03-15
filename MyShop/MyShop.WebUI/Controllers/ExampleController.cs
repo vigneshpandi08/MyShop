f6066145f6764728953ea82ecb1f2d9902a51214
vigneshpandi08 <37330800+vigneshpandi08@users.noreply.github.com>
2018-03-15 16:21:34 +0530

    add a delete request

--- MyShop/MyShop.WebUI/Controllers/ExampleController.cs
+++ MyShop/MyShop.WebUI/Controllers/ExampleController.cs
@@ -17,5 +17,9 @@ namespace MyShop.WebUI.Controllers
         {
             return View();
         }
+        public ActionResult Delete()
+        {
+            return View();
+        }
     }
 }
