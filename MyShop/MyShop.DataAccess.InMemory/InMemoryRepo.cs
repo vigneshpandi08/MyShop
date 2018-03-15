ed526e298acee012d7cfa273e15a365e4485c84b
vigneshpandi08 <37330800+vigneshpandi08@users.noreply.github.com>
2018-03-15 14:52:49 +0530

    add a test controller

---
+++ MyShop/MyShop.DataAccess.InMemory/InMemoryRepo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.InMemory
+{
+    public class InMemoryRepo<T>
+    {
+        ObjectCache cache=MemoryCache.Default;
+        List<T> items;
+        string className;
+
+        public InMemoryRepo()
+        {
+            className = typeof(T).Name;
+        }
+    }
+}
