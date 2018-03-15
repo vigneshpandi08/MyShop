070b7dec2db2c9a5674f8e00c9cda30a46d58693
Bala Chandran <balachandran.sivakumar@dciconsulting.co>
2018-03-15 17:42:27 +0530

    test

--- MyShop/MyShop.WebUI/Controllers/ProductManageController.cs
+++ MyShop/MyShop.WebUI/Controllers/ProductManageController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
+
 using MyShop.Core.Models;
 using MyShop.DataAccess.InMemory;
 using MyShop.Core.ViewModel;
